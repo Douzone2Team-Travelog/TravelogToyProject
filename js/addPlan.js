6daae7dbc8dd81406a701506f03723f61e1de4b4
Lee Jung Min <73169929+LEE-jm96@users.noreply.github.com>
2022-03-24 13:36:17 +0900

    Delete js directory

--- js/addPlan.js
+++
@@ -1,7 +0,0 @@
-$(() => {
-	$('.rightBut').click(function() {
-		var tag = '<div class="form-group section1"><h4 style="width:250px; margin-top: 5px;">시간등록</h4><input type="text" name="timepicker" class="time_element form-control" style="text-align: center;"/><h4 style="position: absolute; left: 30%; margin-top: 0.3%;">일정</h4><input type="text" class="form-control  writePlan"></input></div>';
-		$('.grid').append(tag);
-	});
-	
-});
